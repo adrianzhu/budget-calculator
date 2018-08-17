14b8bdcef5894a17e79b5fcc95409a7ec4d77196
binglett <blng@sfu.ca>
2018-08-17 11:56:18 -0700

    connect to db with uncommitted config file

--- app.js
+++ app.js
@@ -2,12 +2,29 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var logger = require('morgan');
+var sequelize = require('sequelize');
+var pg = require('pg');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var config = require('./config.js');
+
 var app = express();
+
+// db setup
+const connectionString = config.dbinfo;
+const client = new pg.Client(connectionString);
+client.connect(function(err) {
+	if (err) {
+		console.error('Database connection failed: ', err.stack);
+		return;
+	}
+
+	console.log('Connected to Database!');
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -17,6 +34,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
