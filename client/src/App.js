14b36c5b83f4ddb82b24e0fc4415a96bd23bf6fd
binglett <blng@sfu.ca>
2018-10-13 12:37:03 -0700

    added react frontend

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+    state = {users: []}
+
+    // move this somewhere else
+    componentDidMount() {
+        fetch('/users/getadmin')
+        .then(res => res.json())
+        .then(users => this.setState({ users }));
+    }
+
+    render() {
+        return (
+            <div className="App">
+            <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />            
+            </header>            
+            </div>
+            );
+    }
+}
+
+export default App;
